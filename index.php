4b7f611e8b321cf6394bc2584158ebdaf87067f0
klaymen <klaymen@spacebar.hu>
2016-07-17 21:13:12 +0200

    move chapter html code from index to a standalone file (page)

--- index.php
+++ index.php
@@ -42,23 +42,6 @@
       $options .= ("<li><a href=\"".$gb_url."?chapter=".$option_part[1]."\">".$option_part[0]."</a></li>");
     }
   }
-  
-?>
-
-  <header class="image-bg">
-    <img class="img-responsive img-center" src="<?php echo ($gb_images.$img); ?>" alt="<?php echo ($name); ?>">
-  </header>
-  <section>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p class="section-paragraph"><?php echo ($content); ?></p>
-            <div class="forward-options"><ul><?php echo ($options); ?></ul></div>
-        </div>
-      </div>
-    </div>
-  </section>
-  
-<?php
+    include('page.php');
     include('footer.php');
 ?>
---
+++ page.php
@@ -0,0 +1,13 @@
+  <header class="image-bg">
+    <img class="img-responsive img-center" src="<?php echo ($gb_images.$img); ?>" alt="<?php echo ($name); ?>">
+  </header>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="section-paragraph"><?php echo ($content); ?></p>
+            <div class="forward-options"><ul><?php echo ($options); ?></ul></div>
+        </div>
+      </div>
+    </div>
+  </section>
